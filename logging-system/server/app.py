b6b63fcf7aa1b8dfa6441824136445beffd41d27
Juan Castro <juamcastroh@gmil.com>
2026-04-07 01:34:21 -0300

    feat: implementar sistema completo de guaardado de logging asincrono con cola, worker, filtros, junto con endpoints de DELETE /logs por fecha y borrado total con confirmacion

--- logging-system/server/app.py
+++ logging-system/server/app.py
@@ -11,7 +11,16 @@ from database import create_connection    # Funcion para conectar la BD.
 from datetime import datetime             # Manejar timestamps.
 import json                               # Traer tokens dinamicos.
 from pathlib import Path                  # Verificar archivos.
+from queue import Queue                   # Estructura FIFO para guardar logs en BD.
+import threading                          # Permite ejecutar funciones en segundo plano.
 
+
+# ---------------------------------------
+# VARIABLES GLOBALES
+# ---------------------------------------
+
+# --- Cola Global De Logs ---
+log_queue = Queue()
 
 # --- Crear instancia del servidor ---
 app = Flask(__name__)
@@ -54,6 +63,60 @@ def save_token(token, service):
     # --- Guardarlo En El Archivo JSON ---
     with open(TOKENS_FILE, "w") as f:
         json.dump(tokens, f, indent=2)
+
+# ---------------------------------------
+# FUNCION: PARA GUARDR LOG EN LA BD
+# ---------------------------------------
+
+def save_log_to_bd(data):
+
+    # --- Guardar en BD ---
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    # --- Insertar Log ---
+    cursor.execute("""
+    INSERT INTO logs (timestamp, service, severity, message, received_at)
+    VALUES (?, ?, ?, ?, ?)
+    """, (
+        data["timestamp"],
+        data["service"],
+        data["severity"],
+        data["message"],
+        datetime.now().isoformat()
+    ))
+
+    # --- Guardar Cambios ---
+    conn.commit()
+    conn.close()
+
+
+# ----------------------------------------
+# WORKER: PROCESAMIENTO ASINCRONO DE LOGS
+# ----------------------------------------
+
+def log_worker():
+
+    print("[WORKER] Iniciado y esperando logs...")
+
+    # --- Bucle Infinito Para Procesar Logs ---
+    while True:
+
+        log = log_queue.get()          # Obtiene un log de la cola.
+        suceso = False                 # Verificador de fallas.
+
+        for intentos in range(3):
+            try:
+                save_log_to_bd(log)    # Guardar log en BD.
+                suceso = True
+                break
+            except Exception as excep: # Manejo de errores.
+                print(f"[ERROR] Fallo guardado log intento {intentos + 1}: {excep}") 
+
+        if not suceso: # Si fallan los 3 reintentos.
+            print("[CRITICO] Log perdido despues de 3 intentos")
+
+        log_queue.task_done()          # Marcar tarea como completada.
 
 
 # =========================================
@@ -118,32 +181,11 @@ def receive_logs():
         if field not in data:
             return jsonify({"error": f"Flta el campo {field}"}), 400
     
+    # --- Mandar log A La Cola De Guardado ---
 
-    # ---------------------------------------
-    # INSERTAR LOG EN LA BD
-    # ---------------------------------------
+    log_queue.put(data)
 
-    # --- Guardar en BD ---
-    conn = create_connection()
-    cursor = conn.cursor()
-
-    # --- Insertar Log ---
-    cursor.execute("""
-    INSERT INTO logs (timestamp, service, severity, message, received_at)
-    VALUES (?, ?, ?, ?, ?)
-    """, (
-        data["timestamp"],
-        data["service"],
-        data["severity"],
-        data["message"],
-        datetime.now().isoformat()
-    ))
-
-    # --- Guardar Cambios ---
-    conn.commit()
-    conn.close()
-
-    return jsonify({"status": "log guardaado"}), 201
+    return jsonify({"status": "log guardaado"}), 201 # Respuesta inmediata al cliente.
 
 
 # =======================================
@@ -158,10 +200,12 @@ def get_logs():
     # PARAMETRO DE FILTRADO
     # ---------------------------------------
 
-    timestamp_start = request.args.get("timestamp_start")
-    timestamp_end   = request.args.get("timestamp_end")
-    service         = request.args.get("service")
-    severity        = request.args.get("severity")
+    timestamp_start   = request.args.get("timestamp_start")
+    timestamp_end     = request.args.get("timestamp_end")
+    service           = request.args.get("service")
+    severity          = request.args.get("severity")
+    received_at_start = request.args.get("received_at_start")
+    received_at_end   = request.args.get("received_at_end")
 
     # ---------------------------------------
     # CONSTRUCCION DINAMICA DE QUERY
@@ -189,6 +233,14 @@ def get_logs():
     if severity:
         query += " AND severity = ?"
         params.append(severity)
+
+    if received_at_start:
+        query += " AND received_at >= ?"
+        params.append(received_at_start)
+
+    if received_at_end:
+        query += " AND received_at <= ?"
+        params.append(received_at_end)
     
     # --- Ordenar resultados ---
     query += " ORDER BY timestamp DESC"
@@ -276,10 +328,74 @@ def get_stats():
         "last_log_by_service" : last_log_by_service
     })
 
+
+# =======================================
+# ENDPOINT: DELETE /logs
+# Elimina logs antiguos de la base de datos
+# =======================================
+
+@app.route("/logs", methods=["DELETE"])
+def delete_logs():
+
+    # --- Obtener El Corte De Tiempo ---
+    before = request.args.get("before")
+    mode = request.args.get("mode")
+    confirm = request.args.get("confirm")
+
+    # --- Crear Conexion ---
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    # ------------------------
+    # MODO 1: Borrar todo.
+    # ------------------------
+
+    if mode == "all":
+        # --- Proteccion Extra Contra Borrado ---
+        if confirm != "true":
+            return jsonify({
+                "error": "Confirmación requerida para borrar todo (confirm=true)"
+            }), 403
+        cursor.execute("DELETE FROM logs")
+
+    # ------------------------------
+    # MODO 2: Borrar por timestamp.
+
+    # ------------------------------
+    else:
+        # --- Dar Error Si No Hay Fecha Limite ---
+        if not before:
+            return jsonify({"error": "Falta parámetro 'before'"}), 400
+
+        cursor.execute("""
+            DELETE FROM logs
+            WHERE timestamp < ?
+        """, (before,))
+        
+
+    deleted = cursor.rowcount # Cuenta de cuantos registros fueron eliminados.
+
+    # --- Guardar Cambios ---
+    conn.commit()
+    conn.close()
+
+    # --- Respuesta Al Cliente ---
+    return jsonify({
+        "status": "ok",
+        "deleted_logs": deleted # Cantidad de logs borrados.
+    }), 200
+    
+
 # =========================
 # BLOQUE EJECUTOR
 # =========================
 if __name__ == "__main__":
+
+    # --- Iniciar Worker / ---
+    worker_thread = threading.Thread(target=log_worker, daemon=True)
+    worker_thread.start()
+
+    print("[SERVER] Worker iniciado correctamente")
 
     # --- Actualizar Cambios ---
     app.run(debug=True)
